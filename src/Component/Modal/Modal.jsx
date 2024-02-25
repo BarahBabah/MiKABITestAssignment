e3785d49d01d6cf1de611c75106386fd71b99a90
Novel Thunderstorm <novelthunderstorm@gmail.com>
2024-02-25 21:25:45 +0300

    feat: closing the window by esc and overlay

--- src/Component/Modal/Modal.jsx
+++ src/Component/Modal/Modal.jsx
@@ -1,13 +1,39 @@
+import { useRef, useEffect, useCallback } from 'react';
 import styles from './Modal.module.css';
 import PropTypes from 'prop-types';
 
 const Modal = ({ children, setOpenModal }) => {
-    const handleModalClose = () => {
+    const modalRef = useRef();
+
+    const handleModalClose = useCallback(() => {
         setOpenModal(false);
-    };
+    }, [setOpenModal]);
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                handleModalClose();
+            }
+        };
+
+        const handleKeyPress = (event) => {
+            if (event.key === 'Escape') {
+                handleModalClose();
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+        document.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [handleModalClose, setOpenModal]);
+
     return (
         <div className={styles['modal']}>
-            <div className={styles['modal-content']}>
+            <div ref={modalRef} className={styles['modal-content']}>
                 <span className={styles.close} onClick={handleModalClose}>
                     &times;
                 </span>
@@ -16,6 +42,7 @@ const Modal = ({ children, setOpenModal }) => {
         </div>
     );
 };
+
 Modal.propTypes = {
     children: PropTypes.node.isRequired,
     setOpenModal: PropTypes.func.isRequired,
